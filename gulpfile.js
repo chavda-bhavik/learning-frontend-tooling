089c4d9d3f23c0a69195b40b8bcef7f92dae9321
chavda-bhavik <bhavik.chvda@gmail.com>
2021-05-20 17:40:37 +0530

    added tasks to minify and process js, css and check code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-cssnano');
+var prefix = require('gulp-autoprefixer');
+var jshint = require('gulp-jshint');
 
 gulp.task('test', function(done) {
     console.log('Hello World!');
     done()
 });
 
-gulp.task('copy', function(done) {
-    return gulp.src('app/scripts/**/*.js').pipe(gulp.dest('dist'));
-    // done();
+gulp.task('scripts', function() {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'));
 })
+
+gulp.task('styles', function() {
+    return gulp.src('app/styles/main.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(prefix())
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('test', function() {
+    return gulp.src([ 'app/scripts/main.js', '!app/scripts/vendor/**/*.js' ]) // 
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
+});
